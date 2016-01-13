1de4dc6d9cdebb567e7c1afd4670a14f7e9f8c18
sercan <sercan.karaoglu@foreks.com>
2016-01-14 01:04:13 +0200

    csv load scripti eklendi

--- src/loadSpark.R
+++ src/loadSpark.R
@@ -1,5 +1,11 @@
 if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
   Sys.setenv(SPARK_HOME = "/home/rstudio/spark")
 }
+Sys.setenv('SPARKR_SUBMIT_ARGS'='"--packages" "com.databricks:spark-csv_2.10:1.0.3" "sparkr-shell"')
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
 sc <- sparkR.init(master = "local[*]", sparkEnvir = list(spark.driver.memory="2g"))
+
+sqlContext <- sparkRSQL.init(sc)
+
+#load csv
+#train <- read.df(sqlContext, "/home/rstudio/git/homesite/data/train.csv", "com.databricks.spark.csv
